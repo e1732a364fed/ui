3832b6f95539204736813bd5ab31f1a4bff90cb9
e1732a364fed <75717694+e1732a364fed@users.noreply.github.com>
2022-12-18 12:27:58 +0800

    libui-ng: remove 386 support; merge darwin amd and arm to universal.

---
+++ link_darwin.go
@@ -0,0 +1,7 @@
+// 13 december 2015
+
+package ui
+
+// #cgo CFLAGS: -mmacosx-version-min=10.15
+// #cgo LDFLAGS: ${SRCDIR}/libui_darwin_universal.a -framework Foundation -framework AppKit
+import "C"
